577de3f142bca7606b3137536274e373ed6004f5
Eline Van Hooff <e.v.hooff@korsit.com>
2025-05-21 14:31:38 +0200

    feat: enhance image loading with suspense and fallback template

--- app/components/Image.vue
+++ app/components/Image.vue
@@ -11,26 +11,36 @@ const imageTemplate = computed(() => {
 
 <template>
   <!-- <ClientOnly> -->
-  <NuxtImg
-    v-slot="{ src, isLoaded, imgAttrs }"
-    :src="imageTemplate"
-    alt="image"
-    width="400"
-    height="400"
-    :custom="true"
-  >
-    <img
-      v-if="isLoaded"
-      v-bind="imgAttrs"
-      :src="src"
-    >
+  <Suspense>
+    <template #default>
+      <NuxtImg
+        v-slot="{ src, isLoaded, imgAttrs }"
+        :src="imageTemplate || ''"
+        alt="image"
+        width="400"
+        height="400"
+        :custom="true"
+      >
+        <img
+          v-if="isLoaded"
+          v-bind="imgAttrs"
+          :src="src"
+        >
 
-    <img
-      v-else
-      src="https://placehold.co/400x400"
-      alt="placeholder"
-    >
-  </NuxtImg>
+        <img
+          v-else
+          src="https://placehold.co/400x400"
+          alt="placeholder"
+        >
+      </NuxtImg>
+    </template>
+    <template #fallback>
+      <div class="text-center">
+        loading ...
+      </div>
+    </template>
+  </Suspense>
+
   <!-- <img class="contain w-full" :src="imageTemplate || ''" alt="Image" :loading="props.item.name">
   </ClientOnly> -->
 </template>
